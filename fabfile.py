369cf47a43f3957858da6533d37cec3b7a77004d
Sébastien Vaucher <seb.vaucher@gmail.com>
2018-02-01 11:47:56 +0100

    install_usual_packages & zsh

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,19 @@
-from fabric.operations import run
+from fabric.operations import run, sudo
 
 
 def hello():
     run("echo Hello world!")
+
+
+def install_usual_packages():
+    sudo("apt-get -yq update")
+    sudo("apt-get -yq install tmux curl git build-essential mosh ca-certificates curl wget python python3")
+
+
+def zsh():
+    sudo("apt-get -yq update")
+    sudo("apt-get -yq install zsh zsh-antigen wget")
+    run("wget -O ~/.zshrc " +
+        "https://gist.github.com/sebyx31/1adeaa10995e124b4166/raw/5fffc1a28fb070e107d414fff8605ac13ba2253b/.zshrc")
+    user = run("whoami")
+    sudo("chsh -s /bin/zsh " + user)
